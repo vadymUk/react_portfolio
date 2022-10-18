142d6f36dc951717c7390de1147b649dbea308e9
Vadym <vadimcicura@gmail.com>
2022-10-18 05:59:42 +0200

    add basename

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -1,6 +1,6 @@
 import Aside from "../aside/Aside";
 import Menu from "../menu/Menu";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Promo from "../pages/Promo";
 import About from "../pages/About";
 import Portfolio from "../pages/Portfolio";
@@ -11,24 +11,48 @@ import Contacts from "../pages/Contacts";
 import Page404 from "../pages/404";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Aside/>
-        <Menu/>
-          <Routes>
-            <Route path="/" element={<Promo/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/portfolio" element={<Portfolio/>}/>
-            <Route path="/resume" element={<Resume/>}/>
-            <Route path="/skills" element={<Skills/>}/>
-            <Route path="/price" element={<Price/>}/>
-            <Route path="/contacts" element={<Contacts/>}/>
-            <Route path="*" element={<Page404/>} />
-          </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router basename='/react_portfolio'>
+                <Aside />
+                <Menu />
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Promo />}
+                    />
+                    <Route
+                        path='/about'
+                        element={<About />}
+                    />
+                    <Route
+                        path='/portfolio'
+                        element={<Portfolio />}
+                    />
+                    <Route
+                        path='/resume'
+                        element={<Resume />}
+                    />
+                    <Route
+                        path='/skills'
+                        element={<Skills />}
+                    />
+                    <Route
+                        path='/price'
+                        element={<Price />}
+                    />
+                    <Route
+                        path='/contacts'
+                        element={<Contacts />}
+                    />
+                    <Route
+                        path='*'
+                        element={<Page404 />}
+                    />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
